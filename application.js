172f8f12ae87f17c1c7bfbae6ace17c7588a8a03
Trey Jackson <jacksrf@gmail.com>
2014-12-23 16:00:41 -0600

    fixed aboutme section

--- application.js
+++ application.js
@@ -1,3 +1,18 @@
+// $(document).ready(function(){
+//   $('#developeHolder').hide();
+//   $('#designHolder').hide();
+//   $('#innovateHolder').hide();
+//   $('#link1').click(function(){
+//     $('#middle').ScrollTo(function(){
+//       $('#developeHolder').fadeIn(2000, function(){
+//         $('#designHolder').fadeIn(2000,function(){
+//           $('#innovateHolder').fadeIn(2000);
+//           });
+//         });
+//     });
+//   })
+// })
+
 $(document).ready(function(){
     $('#name').fadeIn(3000).animate({letterSpacing: '35px'},2000, function(){
       $('#one').fadeIn(1000, function(){
@@ -7,3 +22,19 @@ $(document).ready(function(){
         });
     });
 });
+
+// $("#aboutMe").click(function() {
+//     $('html, body').animate({
+//         scrollTop: $("#elementtoScrollToID").offset().top
+//     }, 2000);
+// });
+
+// $('#link1').click(function(){
+//   $('#middle').ScrollTo(function(){
+//     $('#developeHolder').fadeIn(2000, function(){
+//       $('#designHolder').fadeIn(2000,function(){
+//         $('#innovateHolder').fadeIn(2000);
+//         });
+//       });
+//   });
+// })
