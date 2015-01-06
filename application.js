3d1fa08bc5876dfd5ec3e8f5622859f41458de99
Trey Jackson <jacksrf@gmail.com>
2015-01-06 17:48:30 -0500

    update

--- application.js
+++ application.js
@@ -14,7 +14,7 @@
 // })
 
 $(document).ready(function(){
-    $('#name').fadeIn(3000).animate({letterSpacing: '35px'},2000, function(){
+    $('#name').fadeIn(1000).animate({letterSpacing: '35px'},2000, function(){
       $('#one').fadeIn(1000, function(){
         $('#two').fadeIn(1000,function(){
           $('#three').fadeIn(1000);
