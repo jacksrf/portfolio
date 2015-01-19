4db958e40f7f45e4431c9f533aea037853889d0f
Trey Jackson <jacksrf@gmail.com>
2015-01-19 18:42:53 -0500

    email test

--- secure_email.php
+++ secure_email.php
@@ -1,14 +1,14 @@
 <?php
- $subject = $_POST['New Contact from Portfolio Website'];
+ $subject = 'New Contact from Portfolio Website'
 
  $message = $_POST['msg'];
 
- $headers = 'From:'. $email2 . "\r\n"; // Sender's Email
+ $headers = 'From:'. $_POST['name'] . $_POST['email'] . "\r\n"; // Sender's Email
 
  // message lines should not exceed 70 characters (PHP rule), so wrap it
  $message = wordwrap($message, 70);
 
  // Send mail by PHP Mail Function
  mail("jacksrf@gmail.com", $subject, $message, $headers);
- echo "Your mail has been sent successfuly ! Thank you for your feedback";
+ 
 ?>
